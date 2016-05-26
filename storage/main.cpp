c17ba54fd6137e1a3f6c9100a2fe7785cc8cff82
jayssj11 <cooljay.gupta@gmail.com>
2016-05-26 20:15:48 +0530

    Create main.cpp

---
+++ storage/main.cpp
@@ -0,0 +1,14 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <fileio.h>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+    return app.exec();
+}
+
