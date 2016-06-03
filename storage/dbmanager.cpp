d465059f4a545d0d08c54db7ee733f211a9ddb85
jayssj11 <cooljay.gupta@gmail.com>
2016-06-03 16:23:56 +0530

    Update dbmanager.cpp

--- storage/dbmanager.cpp
+++ storage/dbmanager.cpp
@@ -4,9 +4,15 @@
 #include <QSqlDriver>
 #include <QCoreApplication>
 #include <QDebug>
-#include <QQmlcomponent>
-#include <QQmlcontext>
-#include <QQmlEngine>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QUrl>
+#include <QUrlQuery>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QByteArray>
+#include <QFile>
 
 
 
@@ -18,17 +24,38 @@ dbmanager::dbmanager(QObject *parent) : QObject(parent)
 void dbmanager::add()
 {
 
-    QQmlEngine engine;
-QQmlComponent component(&engine, "Mywebview.qml");
-QObject *object = component.create();
 
-QVariant returnedValue;
-QMetaObject::invokeMethod(object, "myQmlFunction",Q_RETURN_ARG(QVariant, returnedValue));
 
-qDebug() << "QML function returned:" << returnedValue.toString();
+    // create custom temporary event loop on stack
+       QEventLoop eventLoop;
 
-delete object;
-/*
+       // "quit()" the event-loop, when the network request "finished()"
+       QNetworkAccessManager mgr;
+       QObject::connect(&mgr, SIGNAL(finished(QNetworkReply*)), &eventLoop, SLOT(quit()));
+
+       // the HTTP request
+       QNetworkRequest req( QUrl( QString("http://en.wikitolearn.org/api.php?action=parse&page=Linear%20Algebra/Sets&format=json") ) );
+       QNetworkReply *reply = mgr.get(req);
+       eventLoop.exec();
+
+       if (reply->error() == QNetworkReply::NoError) {
+           //success
+           //qDebug() << "Success" <<reply->readAll();
+          QString   html = (QString)reply->readAll();
+          QJsonDocument jsonResponse = QJsonDocument::fromJson(html.toUtf8());
+
+          QJsonObject jsonObj = jsonResponse.object();
+
+
+         qDebug() << jsonResponse.object()["parse"].toObject()["text"].toObject()["*"].toString();
+         delete reply;
+       }
+       else {
+           //failure
+           qDebug() << "Failure" <<reply->errorString();
+           delete reply;
+       }
+
     QDir databasePath;
     QString path = databasePath.currentPath()+"WTL.db";
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");//not dbConnection
@@ -51,8 +78,7 @@ delete object;
     {
      qDebug() <<query.lastError();
     }
-    */
-    
+
 }
 
 void dbmanager::del()
