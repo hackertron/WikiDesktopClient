c624110af19e52ad9bb107688ac36cc35491425c
jayssj11 <cooljay.gupta@gmail.com>
2016-06-11 14:06:41 +0530

    Update downloader.h

--- storage/downloader.h
+++ storage/downloader.h
@@ -1,32 +1,26 @@
 #ifndef DOWNLOADER_H
 #define DOWNLOADER_H
 
-#include <QObject>
+#include <QFile>
 #include <QNetworkAccessManager>
 #include <QNetworkRequest>
 #include <QNetworkReply>
+#include <QObject>
 #include <QUrl>
-#include <QDateTime>
-#include <QFile>
-#include <QDebug>
 
-
-class Downloader : public QObject
-{
+class Downloader : public QObject {
     Q_OBJECT
+    QFile *m_file;
+    bool m_isReady = true;
+
 public:
-    explicit Downloader(QObject *parent = 0);
+    explicit Downloader(QObject *parent = 0) : QObject(parent) {}
+    virtual ~Downloader() {}
 
-    void doDownload(QString &url);
+    void downloadFileFromURL(const QString &url, const QString &filePath);
 
-signals:
-
-public slots:
-    void replyFinished (QNetworkReply *reply);
-
-private:
-   QNetworkAccessManager *manager;
-
+private slots:
+    void onDownloadFileComplete(QNetworkReply *reply);
 };
 
 #endif // DOWNLOADER_H
