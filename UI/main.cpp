bd8f9d8917407127a6b49a55e39d4dce481f0bef
jayssj11 <cooljay.gupta@gmail.com>
2016-08-11 21:29:25 +0530

    Update main.cpp

--- UI/main.cpp
+++ UI/main.cpp
@@ -17,51 +17,92 @@
 
 
 
+void css_download(QString url , QString filename, QString path)
+{
+    QString requested_url = url;
+    
+    // create custom temporary event loop on stack
+    QEventLoop eventLoop;
+    
+    // "quit()" the event-loop, when the network request "finished()"
+    QNetworkAccessManager mgr;
+    QObject::connect(&mgr, SIGNAL(finished(QNetworkReply*)), &eventLoop, SLOT(quit()));
+    
+    // the HTTP request
+    QNetworkRequest req( requested_url );
+    QNetworkReply *reply = mgr.get(req);
+    eventLoop.exec();
+    
+    if (reply->error() == QNetworkReply::NoError) {
+        
+        QDir dir(path);
+        if (!dir.exists())
+            dir.mkpath(path);
+        if (!dir.exists("WTL_appdata"))
+            dir.mkdir("WTL_appdata");
+        
+        dir.cd("WTL_appdata");
+        
+        QString   css = (QString)reply->readAll();
+        filename = dir.absoluteFilePath(filename);
+        
+        QFile file(filename);
+        if (file.open(QIODevice::ReadWrite)) {
+            QTextStream stream(&file);
+            stream << css << endl;
+        }
+        
+    }
+}
+
 int main(int argc, char *argv[])
 {
     QGuiApplication::setApplicationName("WikiToLearn");
     QGuiApplication::setOrganizationName("en.wikitolearn.org");
     QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
+    
     QGuiApplication app(argc, argv);
-
+    
     QtWebView::initialize();
-
-    qmlRegisterType<dbmanager>("en.wtl.org", 1, 0, "dbmanager");
+    
+    qmlRegisterType<dbmanager>("en.wtl.org", 1, 0, "dbmanager"); // registers the C++ type in the QML system
+    
     dbmanager dbman;
     QQmlApplicationEngine engine;
     engine.rootContext()->setContextProperty( "dbman", &dbman );
-
+    
     qmlRegisterType<listmodel>("en.wtl.model", 1, 0, "model");
     listmodel mod;
-     engine.rootContext()->setContextProperty("mod", &mod);
-
-
-
-   // engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-
-
-
+    engine.rootContext()->setContextProperty("mod", &mod);
+    
+    
+    
+    //standard path to store all the application data ( varies from OS to OS )
     QString path = QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation);
     qDebug() << path;
-
+    
     engine.rootContext()->setContextProperty("path", path);
-
-
-
+    
+    
+    
+    // create wtl application data in application data location ( generic ) 
+    // cd to the newly created application data dir
     QDir dir(path);
     if (!dir.exists())
         dir.mkpath(path);
     if (!dir.exists("WTL_appdata"))
         dir.mkdir("WTL_appdata");
-
+    
     dir.cd("WTL_appdata");
-
-
-
-
-
-
+    
+    
+    
+    
+    /*
+     * creates sqlite DB to keep track of all the stored offline pages
+     * and also it's dependencies 
+     * 
+    */
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");//not dbConnection
     db.setDatabaseName(dir.absoluteFilePath("WTL.db"));
     if(!db.open())
@@ -73,7 +114,7 @@ int main(int argc, char *argv[])
         qDebug() <<"connected" ;
     }
     QSqlQuery query;
-
+    
     if( query.exec("CREATE TABLE IF NOT EXISTS `Pages`"
                    "(  `page_ID` INTEGER NOT NULL PRIMARY KEY ,"
                    "`page_revision` INT  NOT NULL ,"
@@ -85,133 +126,120 @@ int main(int argc, char *argv[])
     {
         qDebug() <<query.lastError();
     }
-
+    
     if(query.exec("CREATE TABLE IF NOT EXISTS `Dependencies` ("
                   "`depe_ID` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
                   " `depe_fileName` VARCHAR(45) NOT NULL,"
                   "`revision_number` INTEGER NOT NULL);"))
     {
         qDebug() << "Dependencies table created";
-
+        
     }
     else
     {
         qDebug() <<query.lastError();
     }
-
+    
     query.clear();
     db.close();
-    QString styling = "<style type=\"text/css\"> "
-
-              " p{"
-
-                 "  display: block;"
-              " -webkit-margin-before: 1em; "
-              " -webkit-margin-after: 1em; "
-              " -webkit-margin-start: 0px; "
-              " -webkit-margin-end: 0px; "
-
-              " } "
-
-
-            " body { "
-           "    font-family: 'Source Sans Pro', sans-serif; "
-            "   } "
-
-        "   pre { "
-         "      display: block; "
-          "     padding: 9.5px; "
-          "     margin: 0 0 10px; "
-           "    font-size: 13px; "
-            "   line-height: 1.42857143; "
-            "   word-break: break-all; "
-            "   word-wrap: break-word; "
-             "  color: #333; "
-           "    background-color: #f5f5f5; "
-            "   border: 1px solid #ccc; "
-            "   border-radius: 4px; "
-        "   } "
-
-
-
-         "  </style> "
-            "";
-
-
-
-     QString filename = dir.absoluteFilePath("main.css");
-     qDebug() << filename;
-     QFile file(filename);
-           if (file.open(QIODevice::ReadWrite)) {
-               QTextStream stream(&file);
-               stream << styling << endl;
-           }
-
- //   QString neverland  = dir.absoluteFilePath("menu.png");
- //   QFile::copy(":/images/menu.png",neverland);
-
-
-/* model view shit goes here */
-
-
-
-
-                          if(!db.open())
-                          {
-                              qDebug() <<"error in opening DB";
-                          }
-                          else
-                          {
-                              qDebug() <<"connected to DB" ;
-
-                          }
-
-                          QVector<QString> page_id;
-                          QVector<QString> page_title;
-
-                          QSqlQuery quer("SELECT page_ID , page_title FROM Pages");
-                          while (quer.next()) {
-                              QString i = quer.value(0).toString();
-                              page_id.push_back(i);
-                              QString p = quer.value(1).toString();
-                              page_title.push_back(p);
-
-
-                          }
-                          for (int i = 0; i < page_id.size(); ++i)
-                              {
-                               qDebug() << page_id.at(i).toLocal8Bit().constData() << endl;
-                               qDebug() << page_title.at(i).toLocal8Bit().constData() << endl;
-                                mod.addpages(list(page_title.at(i) , page_id.at(i)));
-
-
-
-
-                              }
-
-
-
-
-
-                      QQmlContext *ctxt = engine.rootContext();
-                      ctxt->setContextProperty("myModel", &mod);
-
-
-/* model view shit ends here */
-
-
-
+    
+    
+   
+    
+    /* 
+     * Populate the model to show all the available offline pages ( IF ANY )
+     * this model list will be showed to user so that he/she can 
+     * either read the offline page or delete or update them
+     * 
+    */   
+    
+    
+    
+    if(!db.open())
+    {
+        qDebug() <<"error in opening DB";
+    }
+    else
+    {
+        qDebug() <<"connected to DB" ;
+        
+    }
+    
+    QVector<QString> page_id;
+    QVector<QString> page_title;
+    
+    QSqlQuery quer("SELECT page_ID , page_title FROM Pages");
+    while (quer.next()) {
+        QString i = quer.value(0).toString();
+        page_id.push_back(i);
+        QString p = quer.value(1).toString();
+        page_title.push_back(p);
+        
+        
+    }
+    for (int i = 0; i < page_id.size(); ++i)
+    {
+        qDebug() << page_id.at(i).toLocal8Bit().constData() << endl;
+        qDebug() << page_title.at(i).toLocal8Bit().constData() << endl;
+        mod.addpages(list(page_title.at(i) , page_id.at(i)));
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    QQmlContext *ctxt = engine.rootContext();
+    ctxt->setContextProperty("myModel", &mod);
+    
+    
+  
+    
+    /* *** css download **/
+    
+    if(QFile::exists(dir.absoluteFilePath("main.css"))) // checks if the css already exist 
+    {
+        qDebug() << " already downloaded main.css";
+    }
+    
+    else{
+        QString url = "http://en.wikitolearn.org/load.php?debug=false&lang=en&modules=ext.gadget.ColiruCompiler%7Cext.math.desktop.styles%7Cext.math.styles%7Cext.visualEditor.desktopArticleTarget.noscript%7Cmediawiki.legacy.commonPrint%2Cshared%7Cmediawiki.sectionAnchor&only=styles&skin=neverland";
+        css_download(url,"main.css",path);
+        
+    }
+    
+    if(QFile::exists(dir.absoluteFilePath("bootstrap.css")))
+    {
+        qDebug() << "already downloaded bootstrap.css";
+    }
+    else{
+        QString  url = "http://en.wikitolearn.org/skins/Neverland/css/bootstrap.css?v=1.0.3?303";
+        css_download(url,"bootstrap.css",path);
+        
+    }
+    
+    
+    
+    
+    
+    
+    /* css download end  */
+    
     QSettings settings;
     QString style = QQuickStyle::name();
     if (!style.isEmpty())
         settings.setValue("style", style);
     else
         QQuickStyle::setStyle(settings.value("style").toString());
-
-
+    
+    
+    //load main.qml and stores the settings 
     engine.load(QUrl("qrc:/main.qml"));
     if (engine.rootObjects().isEmpty())
         return -1;
-
+    
     return app.exec();
 }
